a0aaa800178741591a29670a5bfe38af7cb95e5f
Alejo <alejo@Homes-iMac.local>
2012-12-24 17:54:25 +1100

    Created model for MVC project.

---
+++ Navigation/NavigationModel.h
@@ -0,0 +1,13 @@
+//
+//  NavigationModel.h
+//  Navigation
+//
+//  Created by Alejo on 24/12/12.
+//  Copyright (c) 2012 Alejo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NavigationModel : NSObject
+
+@end
---
+++ Navigation/NavigationModel.m
@@ -0,0 +1,13 @@
+//
+//  NavigationModel.m
+//  Navigation
+//
+//  Created by Alejo on 24/12/12.
+//  Copyright (c) 2012 Alejo. All rights reserved.
+//
+
+#import "NavigationModel.h"
+
+@implementation NavigationModel
+
+@end
