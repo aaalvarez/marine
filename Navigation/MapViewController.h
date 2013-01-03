752fb4c0adbcafd1f7abdbde0511c3960f8d2bbb
Alejo <alejo@Homes-iMac.local>
2013-01-03 20:33:55 +1100

    Created the controller for the new view and set the ids for all the view controllers.

---
+++ Navigation/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Navigation
+//
+//  Created by Alejo on 2/01/13.
+//  Copyright (c) 2013 Alejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
